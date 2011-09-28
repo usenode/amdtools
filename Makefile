0035a0d62eddaacce71ea6b867f9cdb6aa01771a
Tom Yandell <tom@yandell.me.uk>
2011-09-28 22:06:32 +0100

    clean pu

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
 lib/amdtools.js: lib/amdtools.src.js
-	chmod +w lib/amdtools.js && (perl -e 'undef $$/; $$_ = <STDIN>; s{rule{(.*?)}x}{$$a=$$1;$$a =~ s{//.*?$$}{}gm; $$a =~ s/\s+//gs; $$a =~ s/\\h/[ \\t]/g; $$a =~ s{/}{\\/}g; "/$$a/g"}sge; print' < lib/amdtools.src.js > lib/amdtools.js) && chmod -w lib/amdtools.js
+	touch lib/amdtools.js && chmod +w lib/amdtools.js && (perl -e 'undef $$/; $$_ = <STDIN>; s{rule{(.*?)}x}{$$a=$$1;$$a =~ s{//.*?$$}{}gm; $$a =~ s/\s+//gs; $$a =~ s/\\h/[ \\t]/g; $$a =~ s{/}{\\/}g; "/$$a/g"}sge; print' < lib/amdtools.src.js > lib/amdtools.js) && chmod -w lib/amdtools.js
 
 ./node_modules/.bin/litmus:
 	npm install litmus
--- tests/conversions.js
+++ tests/conversions.js
@@ -5,20 +5,79 @@ var litmus = require('litmus'),
 exports.test = new litmus.Test('amd conversions test', function () {
     var test = this;
 
-    test.plan(2);
+    test.plan(8);
 
-    test.is(
-        amdtools.commonJsToAmd('// body javascript'),
-        'define(["require","exports","module"],function(require,exports,module){// body javascript\n});',
+    function testCommonJsToAmd (body, options, namePart, dependenciesPart, name) {
+        test.is(
+            amdtools.commonJsToAmd(body, options),
+            'define(' + namePart + '["require","exports","module"' + dependenciesPart + '],function(require,exports,module){' + body + '\n});',
+            name
+        );
+    }
+
+    testCommonJsToAmd(
+        '// body javascript',
+        {},
+        '',
+        '',
         'basic amd conversion'
     );
 
-    test.is(
-        amdtools.commonJsToAmd('require("blah");'),
-        'define(["require","exports","module","blah"],function(require,exports,module){require("blah");\n});',
-        'amd conversion with embedded require call'
+    testCommonJsToAmd(
+        'require(\'foo\')',
+        {},
+        '',
+        ',"foo"',
+        'amd conversion with require call'
     );
 
+    testCommonJsToAmd(
+        'require(\'foo\');require("bar");require("\\x66\\x6f\\x6f");require(\'bar\');',
+        {},
+        '',
+        ',"bar","foo"',
+        'duplicate require calls removed and list determinate'
+    );
+
+    testCommonJsToAmd(
+        'require("foo"); /* require("bar"); */',
+        {},
+        '',
+        ',"foo"',
+        'require in multiline comments ignored'
+    );
+
+    testCommonJsToAmd(
+        'require("foo"); // require("bar");\n// require("baz");\n require("baz")',
+        {},
+        '',
+        ',"baz","foo"',
+        'require in single line comments ignored'
+    );
+
+    testCommonJsToAmd(
+        'require("foo"); if (/require("bar")/i.test(""));',
+        {},
+        '',
+        ',"foo"',
+        'require in RegExp ignored'
+    );
+
+    testCommonJsToAmd(
+        'require("foo"); \'require("bar");\'; "require(\'baz\');";',
+        {},
+        '',
+        ',"foo"',
+        'require in string literals ignored'
+    );
+
+    testCommonJsToAmd(
+        'require("foo")',
+        { name: 'bar' },
+        '"bar",',
+        ',"foo"',
+        'can provide optional name'
+    );
 });
 
 
