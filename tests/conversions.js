ab6bcb50bad9cc31ba37f91381e5b85c779a7cd1
Tom Yandell <tom@yandell.me.uk>
2011-10-02 23:01:55 +0100

    add require to generated boilerplate for amd to commonjs conversion

--- tests/conversions.js
+++ tests/conversions.js
@@ -5,7 +5,7 @@ var litmus = require('litmus'),
 exports.test = new litmus.Test('amd conversions test', function () {
     var test = this;
 
-    test.plan(26);
+    test.plan(30);
 
     function testCommonJsToAmd (body, options, namePart, dependenciesPart, name) {
         test.is(
@@ -106,9 +106,9 @@ exports.test = new litmus.Test('amd conversions test', function () {
     // this cheats a little by re-evaluating the passed in function in a new context, to allow access
     // to a local "define", "mainRequire", "mainExports" and "mainModule" variables to test with
     function testAmdToCommonJs (runner, checker) {
-        var required    = [],
+        var required    = {},
             require     = function (what) {
-                required.push(what);
+                required[what] = 1;
                 return { name: what };
             },
             mainRequire = require,
@@ -129,7 +129,7 @@ exports.test = new litmus.Test('amd conversions test', function () {
             });
         },
         function (required, exported) {
-            test.is(required.length, 0, 'nothing required by default');
+            test.is(Object.keys(required).length, 0, 'nothing required by default');
             test.is(Object.keys(exported).length, 0, 'nothing exported by default');
         }
     );
@@ -143,7 +143,7 @@ exports.test = new litmus.Test('amd conversions test', function () {
             });
         },
         function (required, exported) {
-            test.is(required.length, 0, 'nothing required by default for named define');
+            test.is(Object.keys(required).length, 0, 'nothing required by default for named define');
             test.is(Object.keys(exported).length, 0, 'nothing exported by default for named define');
         }
     );
@@ -157,8 +157,23 @@ exports.test = new litmus.Test('amd conversions test', function () {
             });
         },
         function (required, exported) {
-            test.is(required, ['b', 'c', 'a'], 'require called for each unique dependency');
+            test.is(required, { a: 1, b: 1, c: 1 }, 'require called for each unique dependency');
             test.is(Object.keys(exported).length, 0, 'nothing exported by default when dependencies loaded');
+        }
+    );
+
+    testAmdToCommonJs(
+        function () {
+            require(['foo', 'bar'], function (a, b) {
+                test.is(a.name, 'foo', 'async style require outside of define');
+                test.is(b.name, 'bar', 'async style require outside of define 2');
+                define(['require', 'baz'], function (require) {
+                    test.is(require('baz').name, 'baz', 'synchronously require within callback');
+                });
+            });
+        },
+        function (required, exported) {
+            test.is(required, { foo: 1, bar: 1, baz: 1 }, 'async and sync-style require called');
         }
     );
 });
