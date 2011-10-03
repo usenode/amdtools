81ab2ebee7876ad15501fa42f497b3df3576bd83
Tom Yandell <tom@yandell.me.uk>
2011-10-03 08:12:07 +0100

    Support a minimal wrapper during amd to commonjs conversion that depends on amdtools, as well as the standalone version.

--- tests/conversions.js
+++ tests/conversions.js
@@ -5,7 +5,7 @@ var litmus = require('litmus'),
 exports.test = new litmus.Test('amd conversions test', function () {
     var test = this;
 
-    test.plan(30);
+    test.plan(49);
 
     function testCommonJsToAmd (body, options, namePart, dependenciesPart, name) {
         test.is(
@@ -106,74 +106,82 @@ exports.test = new litmus.Test('amd conversions test', function () {
     // this cheats a little by re-evaluating the passed in function in a new context, to allow access
     // to a local "define", "mainRequire", "mainExports" and "mainModule" variables to test with
     function testAmdToCommonJs (runner, checker) {
-        var required    = {},
-            require     = function (what) {
-                required[what] = 1;
-                return { name: what };
-            },
-            mainRequire = require,
-            exports     = {},
-            mainExports = exports,
-            module      = { id: 'name' }
-            mainModule  = module;
-        eval(amdtools.amdToCommonJs('(' + runner.toString() + ')();'));
-        checker(required, exports);
+        var tester = function (runner, checker, standalone) {
+            var required          = {},
+                require           = function (what) {
+                    if (what === 'amdtools') {
+                        return amdtools;
+                    }
+                    required[what] = 1;
+                    return { name: what };
+                },
+                mainRequire       = require,
+                exports           = {},
+                mainExports       = exports,
+                module            = { id: 'name' }
+                mainModule        = module,
+                standalonePostfix = standalone ? ' (standalone)' : '';
+            eval(amdtools.amdToCommonJs('(' + runner.toString() + ')();', { standalone: standalone }));
+            checker(required, exports, standalonePostfix);
+        };
+        tester(runner, checker);
+        tester(runner, checker, true);
     }
 
     testAmdToCommonJs(
         function () {
             define(function (require, exports, module) {
-                test.is(require, mainRequire, 'default first parameter is require');
-                test.is(exports, mainExports, 'default second parameter is exports');
-                test.is(module, mainModule, 'default third parameter is module');
+                test.is(require, mainRequire, 'default first parameter is require' + standalonePostfix);
+                test.is(exports, mainExports, 'default second parameter is exports' + standalonePostfix);
+                test.is(module, mainModule, 'default third parameter is module' + standalonePostfix);
             });
         },
-        function (required, exported) {
-            test.is(Object.keys(required).length, 0, 'nothing required by default');
-            test.is(Object.keys(exported).length, 0, 'nothing exported by default');
+        function (required, exported, standalonePostfix) {
+            test.is(Object.keys(required).length, 0, 'nothing required by default' + standalonePostfix);
+            test.is(Object.keys(exported).length, 0, 'nothing exported by default' + standalonePostfix);
         }
     );
 
     testAmdToCommonJs(
         function () {
             define('name', function (require, exports, module) {
-                test.is(require, mainRequire, 'default first parameter is require for named define');
-                test.is(exports, mainExports, 'default second parameter is exports for named define');
-                test.is(module, mainModule, 'default third parameter is module for named define');
+                test.is(require, mainRequire, 'default first parameter is require for named define' + standalonePostfix);
+                test.is(exports, mainExports, 'default second parameter is exports for named define' + standalonePostfix);
+                test.is(module, mainModule, 'default third parameter is module for named define' + standalonePostfix);
             });
         },
-        function (required, exported) {
-            test.is(Object.keys(required).length, 0, 'nothing required by default for named define');
-            test.is(Object.keys(exported).length, 0, 'nothing exported by default for named define');
+        function (required, exported, standalonePostfix) {
+            test.is(Object.keys(required).length, 0, 'nothing required by default for named define' + standalonePostfix);
+            test.is(Object.keys(exported).length, 0, 'nothing exported by default for named define' + standalonePostfix);
         }
     );
 
     testAmdToCommonJs(
         function () {
             define(['b', 'c', 'a'], function (first, second, third) {
-                test.is(first.name, 'b', 'first dependency loaded');
-                test.is(second.name, 'c', 'second dependency loaded');
-                test.is(third.name, 'a', 'third dependency loaded');
+                test.is(first.name, 'b', 'first dependency loaded' + standalonePostfix);
+                test.is(second.name, 'c', 'second dependency loaded' + standalonePostfix);
+                test.is(third.name, 'a', 'third dependency loaded' + standalonePostfix);
             });
         },
-        function (required, exported) {
-            test.is(required, { a: 1, b: 1, c: 1 }, 'require called for each unique dependency');
-            test.is(Object.keys(exported).length, 0, 'nothing exported by default when dependencies loaded');
+        function (required, exported, standalonePostfix) {
+            test.is(required, { a: 1, b: 1, c: 1 }, 'require called for each unique dependency' + standalonePostfix);
+            test.is(Object.keys(exported).length, 0, 'nothing exported by default when dependencies loaded' + standalonePostfix);
         }
     );
 
     testAmdToCommonJs(
         function () {
             require(['foo', 'bar'], function (a, b) {
-                test.is(a.name, 'foo', 'async style require outside of define');
-                test.is(b.name, 'bar', 'async style require outside of define 2');
+                test.is(a.name, 'foo', 'async style require outside of define' + standalonePostfix);
+                test.is(b.name, 'bar', 'async style require outside of define 2' + standalonePostfix);
                 define(['require', 'baz'], function (require) {
-                    test.is(require('baz').name, 'baz', 'synchronously require within callback');
+                    test.is(require('baz').name, 'baz', 'synchronously require within callback' + standalonePostfix);
                 });
             });
         },
-        function (required, exported) {
-            test.is(required, { foo: 1, bar: 1, baz: 1 }, 'async and sync-style require called');
+        function (required, exported, standalonePostfix) {
+            test.is(required, { foo: 1, bar: 1, baz: 1 }, 'async and sync-style require called' + standalonePostfix);
         }
     );
 });
